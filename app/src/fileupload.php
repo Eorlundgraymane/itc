538af97f1d2d2ceccaf310084db4e5cf30f63b82
Eorlundgraymane <rkmenon235@gmail.com>
2017-10-18 16:15:26 +0530

    ITC Project Day Progress

--- app/src/fileupload.php
+++ app/src/fileupload.php
@@ -1,8 +1,10 @@
-<div id = "fileDiv" ng-app = "fileApp" ng-controller = "fileCtrl">
-  <input class = "btn btn-default" ng-model = "text" onchange="angular.element(this).scope().showContent(this)" type = "file" accept = "text/*">
-  <div ng-if = "content" class = "form-group col-xs-12 col-lg-12 col-md-12 col-sm-12">
-    <label for = "inputtext">File content is :</label>
-      <textarea class = "form-control" rows = "15" id = "inputtext" >{{content}}</textarea>
-      <button type = "button" class = "btn btn-default" ng-click = "analyze();">Analyze</button>
+<div class = "row">
+  <div class = "well col-xs-12 col-md-12 col-sm-12 col-lg-12" id = "fileDiv" ng-app = "fileApp" ng-controller = "fileCtrl">
+    <input class = "btn btn-default" ng-model = "text" onchange="angular.element(this).scope().showContent(this)" type = "file" accept = "text/*">
+    <div ng-if = "content" class = "form-group col-xs-12 col-lg-12 col-md-12 col-sm-12">
+      <label for = "inputtext">File content is :</label>
+        <textarea class = "form-control" rows = "15" id = "inputtext" >{{content}}</textarea>
+        <button type = "button" class = "btn btn-default" ng-click = "analyze();">Analyze</button>
+    </div>
   </div>
 </div>
